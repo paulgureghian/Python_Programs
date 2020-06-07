ffe40053cdef61739f7230e872d7024e19334893
paulgureghian <paulgureghian@att.net>
2020-06-06 19:16:35 -0700

    Added a Python program file

---
+++ PyCaret.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+
+# Created by Paul A. Gureghian in June 2020. #
+# This Python program demos the PyCaret ML library. #
+
+# Start the program. # 
+
+# Import PyCaret. #
+from pycaret.datasets import get_data
+diabetes = get_data('diabetes')
