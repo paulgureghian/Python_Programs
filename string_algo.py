0714211540f441de9454f484074c23e93bb3e37b
PaulGureghian <paulgureghian@att.net>
2020-09-21 13:18:04 -0700

    Made print statement edits

--- string_algo.py
+++ string_algo.py
@@ -1,23 +1,23 @@
 # An algorithm which takes a string
-# And uses a loop and conditionals to find certain letters in the string 
+# And uses a loop and conditionals to find certain letters in the string
 
 # Initialize variables
 an_letters = "aefhilmnorsxAEFHILMNORSX"
 print("")
 word = input("Enter a word: ")
-times = int(input("Level (1-10): "))
+times = int(input("Times (1-10): "))
 
-# For loop through the inputted word 
+# For loop through the inputted word
 for char in word:
 
     if char in an_letters:
         print("Give me an " + char)
-        print("")
 
     else:
         print("")
-        print("Give me a " + char)
+        print("Give me a  " + char)
         print("")
 
 for i in range(times):
+    print("")
     print(word, "\n")
