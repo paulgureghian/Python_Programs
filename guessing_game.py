2d2f2865d974b7af1a9c17af45911bbbff067fc1
PaulGureghian <paulgureghian@att.net>
2020-10-06 18:14:23 -0700

    Added source code file program

---
+++ guessing_game.py
@@ -0,0 +1,16 @@
+import random
+
+
+def start_game():
+
+    print("")
+    print("---------------------------------------")
+    print("Welcome to the Number Guessing Game!")
+    print("---------------------------------------")
+    print("")
+
+    number = input("Enter a number between 1 and 10: ")
+    print("")
+    
+    
+start_game()
