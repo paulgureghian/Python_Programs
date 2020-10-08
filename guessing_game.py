dbb244c6cb28254283a6986823a0d83b15ac3d17
PaulGureghian <paulgureghian@att.net>
2020-10-07 18:05:58 -0700

    Added logic to main()

--- guessing_game.py
+++ guessing_game.py
@@ -3,8 +3,16 @@
 # Import library
 import random
 
+# Generate a new random number
+def new_rand_num_generator():
+
+    return random.randint(1, 10)
+
 # Define 'start_game' function
+
 def start_game():
+
+    number_of_attempts = 0;
 
     print("")
     print("---------------------------------------")
@@ -12,8 +20,22 @@ def start_game():
     print("---------------------------------------")
     print("")
 
-    number = input("Enter a number between 1 and 10: ")
+    # Generate a randon number between 1 - 10
+    rand_num = new_rand_num_generator()
+    
+    # Get user number
+    number = int(input("Enter a number between 1 and 10: "))
     print("")
-    
-    
+
+    if number > rand_num:
+        print("It's lower")
+
+    elif number < rand_num:
+        print("It's higher")
+
+    else:
+        print("Got it")
+        
+    print("The game is over\n")    
+
 start_game()
