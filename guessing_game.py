b6377bbef6b942af622ba2a4f854ef9fcc4e0321
PaulGureghian <paulgureghian@att.net>
2020-10-08 17:49:59 -0700

    Made edits

--- guessing_game.py
+++ guessing_game.py
@@ -23,6 +23,7 @@ def start_game():
 
     # Generate a randon number between 1 - 10
     rand_num = new_rand_num_generator()
+    print("Random number is:", rand_num, "\n")
 
     # Get user number
     number = int(input("Enter a number between 1 and 10: "))
@@ -33,20 +34,21 @@ def start_game():
         try:
             
             number = int(input("Enter a number between 1 and 10: "))
-
+            print("")
+            
             if number > rand_num:
                 number_of_attempts += 1
                 print("It's lower")
-                
+                print("")                
 
             elif number < rand_num:
                 number_of_attempts += 1
                 print("It's higher")
-                
+                print("")
 
             else:
                 number_of_attempts += 1
-                print("You got it , it took you",number_of_attempts, "attempts")
+                print("You got it, it took you",number_of_attempts, "attempts")
                 print("")
                 print("The game is over\n")
                 break
